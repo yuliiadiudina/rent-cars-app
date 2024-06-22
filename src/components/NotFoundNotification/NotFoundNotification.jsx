da1afd59e7f04048f728c35b2a41269a12f16d59
Yuliia Diudina <yuliiadiudina@gmail.com>
2024-06-23 00:29:51 +0300

    stylized not found page

--- src/components/NotFoundNotification/NotFoundNotification.jsx
+++ src/components/NotFoundNotification/NotFoundNotification.jsx
@@ -1,9 +1,11 @@
+import css from './NotFoundNotification.module.css'
+
 function NotFoundNotification() {
   return (
-    <>
-      <h3>Oops! We can't find the page you're looking for</h3>
-      <h4>You tried to request a page that doesn't exist </h4>
-    </>
+    <section className={css.sectionNotFound}>
+      <h3 className={css.sectionNotFoundTitle}>Oops! We can't find the page you're looking for &#128549;</h3>
+      <h4 className={css.sectionNotFoundTitle}>You tried to request a page that doesn't exist</h4>
+    </section>
   );
 }
 export default NotFoundNotification;
